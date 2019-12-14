1879bcc765332f73e39257d668e46902dc3b9371
AmineKh14 <khamouda123@gmail.com>
2019-12-14 14:19:43 +0100

    Data base init

--- Main/admin.py
+++ Main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Utilisateur)
+admin.site.register(Profile)
+admin.site.register(Historique)
+
+
 
 # Register your models here.
--- Main/models.py
+++ Main/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.template.defaultfilters import slugify
+from django.urls import reverse
+from django.conf import settings    
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+ROLE = (
+        ('admin', 'Admin'),
+        ('client', 'Client'),
+        ('freelancer','Freelancer'),
+    )
+
+JOB = (
+        ('1cpi', '1CPI'),
+        ('2cpi', '2CPI'),
+        ('1cs', '1CS'),
+        ('2cs', '2CS'),
+        ('3cs', '3CS'),
+    )
+
+STATE = (
+    ('Occupé','occupé'),
+    ('Libre','libre')
+)
+
+class Utilisateur(AbstractUser):
+    role = models.CharField(choices=ROLE,default='etudiant',max_length=10)
+    banned=models.BooleanField(default=False)
+
+
+class Profile (models.Model):
+    
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name="profile")
+    numero_telephone = models.IntegerField(null=True)
+    job = models.CharField(choices=JOB,default='1cpi',max_length=4,null=True)
+    bio = models.TextField(null=True,blank=True)
+    image = models.ImageField(null=True,upload_to='profile_pics',default='profile_pics/default.png')
+    state = models.BooleanField(choices=STATE)
+    rating = models.IntegerField()
+
+class Historique(models.Model):
+    client = models.OneToOneField(Utilisateur,on_delete=models.CASCADE,related_name="client")
+    freelancer = models.OneToOneField(Utilisateur,on_delete=models.CASCADE,related_name="freelancer")
+    dateD = models.DateField()
+    dateF = models.DateField()
+
+
